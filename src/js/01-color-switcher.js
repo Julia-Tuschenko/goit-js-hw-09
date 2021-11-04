fb5a16d17e6655e45a276c5640f75d1500c29589
Julia Tushchenko <juliayulianna@gmail.com>
2021-11-04 12:35:31 +0200

    start

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    colorBody: document.querySelector('body'),
+};
+
+console.log(refs.btnStart);
+console.log(refs.btnStop);
+
+
+function onColorImprovisation(){
+    const color = getRandomHexColor();
+    refs.colorBody.style.backgroundColor = color;
+    spanText.textContent = color;
+}
+
+refs.btnStart.addEventListener("click", onColorImprovisation);
